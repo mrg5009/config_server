cec574dc14750e1537fcd3f87970fd0e17c0da95
Mohamadreza <175953983+mrg5009@users.noreply.github.com>
2024-10-22 20:30:31 +0330

    Update open_ssh.bat

--- open_ssh.bat
+++ open_ssh.bat
@@ -1,3 +1,3 @@
-nmap -p 3000 --scanflags SYN 194.116.216.20
-nmap -p 4000 --scanflags SYN 194.116.216.20
-nmap -p 5000 --scanflags SYN 194.116.216.20
+nmap -p 3000 --scanflags SYN <ip-server>
+nmap -p 4000 --scanflags SYN <ip-server>
+nmap -p 5000 --scanflags SYN <ip-server>
