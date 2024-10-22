2120afa6ba5d2e1e17d50240e57d5f18d592a6b1
Mohamadreza <175953983+mrg5009@users.noreply.github.com>
2024-10-22 20:27:42 +0330

    Update close_http.bat

--- close_http.bat
+++ close_http.bat
@@ -1,3 +1,3 @@
-nmap -p 1600 --scanflags SYN 194.116.216.20
-nmap -p 1700 --scanflags SYN 194.116.216.20
-nmap -p 1800 --scanflags SYN 194.116.216.20
+nmap -p 1600 --scanflags SYN <ip-server>
+nmap -p 1700 --scanflags SYN <ip-server>
+nmap -p 1800 --scanflags SYN <ip-server>
