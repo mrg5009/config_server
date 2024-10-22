2e3c25387b6b75d4b3e3bec9c059e788aa971caa
Mohamadreza <175953983+mrg5009@users.noreply.github.com>
2024-10-22 20:30:17 +0330

    Update open_socks.bat

--- open_socks.bat
+++ open_socks.bat
@@ -1,3 +1,3 @@
-nmap -p 7000 --scanflags SYN 194.116.216.20
-nmap -p 8000 --scanflags SYN 194.116.216.20
-nmap -p 9000 --scanflags SYN 194.116.216.20
+nmap -p 7000 --scanflags SYN <ip-server>
+nmap -p 8000 --scanflags SYN <ip-server>
+nmap -p 9000 --scanflags SYN <ip-server>
