a254f15e200f973815a064db6ddd1974c9e2e142
Mohamadreza <175953983+mrg5009@users.noreply.github.com>
2024-10-22 20:29:09 +0330

    Update close_socks.bat

--- close_socks.bat
+++ close_socks.bat
@@ -1,3 +1,3 @@
-nmap -p 1000 --scanflags SYN 194.116.216.20
-nmap -p 1100 --scanflags SYN 194.116.216.20
-nmap -p 1200 --scanflags SYN 194.116.216.20
+nmap -p 1000 --scanflags SYN <ip-server>
+nmap -p 1100 --scanflags SYN <ip-server>
+nmap -p 1200 --scanflags SYN <ip-server>
